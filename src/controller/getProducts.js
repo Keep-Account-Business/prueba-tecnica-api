bcdcd314cb979a0f15630381a6e1df48aee239b4
yohaan2 <garciayohan57@gmail.com>
2024-04-08 13:22:46 -0400

    refactor indentation

--- src/controller/getProducts.js
+++ src/controller/getProducts.js
@@ -13,8 +13,8 @@ export const getProducts = async (req, res) => {
       }
     });
     return res
-            .status(200)  
-            .json(products);
+      .status(200)  
+      .json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -25,14 +25,14 @@ export const getProductById = async (req, res) => {
     const response = await axios.get(`https://fakestoreapi.com/products/${req.params.id}`);
     const product = response.data;
     return res
-            .status(200)  
-            .json({
-              title: product.title,
-              price: product.price,
-              description: product.description,
-              category: product.category,
-              image: product.image,
-            });
+      .status(200)  
+      .json({
+        title: product.title,
+        price: product.price,
+        description: product.description,
+        category: product.category,
+        image: product.image,
+      });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -51,8 +51,8 @@ export const getProductsByCategory = async (req, res) => {
       }
     });
     return res
-            .status(200)  
-            .json(products);
+      .status(200)  
+      .json(products);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
