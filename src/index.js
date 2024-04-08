2378a169e798eda0c40ccbad1ee76747b25cc70c
yohaan2 <garciayohan57@gmail.com>
2024-04-08 19:37:42 -0400

    update port to use PORT environment variable or 3000 by default

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import { router } from './routes/index.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use((req, res, next) => {
