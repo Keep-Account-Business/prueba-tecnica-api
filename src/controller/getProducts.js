c3723cb2ebba7fd6353a33ddd72df017ac5d0d22
yohaan2 <garciayohan57@gmail.com>
2024-04-08 15:40:58 -0400

    add getCategories endpoint

--- src/controller/getProducts.js
+++ src/controller/getProducts.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getProducts = async (req, res) => {
   try {
-    const response = await axios.get('https://fakestoreapi.com/products');
+    const response = await axios.get(`https://fakestoreapi.com/products`);
     const products = response.data.map((product) => {
       return {
         title: product.title,
@@ -16,13 +16,14 @@ export const getProducts = async (req, res) => {
       .status(200)  
       .json(products);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 };
 
 export const getProductById = async (req, res) => {
   try {
-    const response = await axios.get(`https://fakestoreapi.com/products/${req.params.id}`);
+    const { id } = req.params;
+    const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
     const product = response.data;
     return res
       .status(200)  
@@ -34,13 +35,22 @@ export const getProductById = async (req, res) => {
         image: product.image,
       });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 };
 
 export const getProductsByCategory = async (req, res) => {
   try {
-    const response = await axios.get(`https://fakestoreapi.com/products/category/${req.params.category}`);
+    const { category } = req.query;
+    const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"];
+
+    if (!categories.includes(category)) {
+      return res
+        .status(400)
+        .json({ message: "Invalid category" });
+    }
+
+    const response = await axios.get(`https://fakestoreapi.com/products/category/${category}`);
     const products = response.data.map((product) => {
       return {
         title: product.title,
@@ -54,6 +64,18 @@ export const getProductsByCategory = async (req, res) => {
       .status(200)  
       .json(products);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const getCategories = async (req, res) => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products/categories');
+    const categories = response.data;
+    return res
+      .status(200)  
+      .json(categories);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
 };
